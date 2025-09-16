9c97777139abb6ff17e5beddeef0a35d90ce30dd
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-09-16 15:01:34 +0900

    ci: fix

--- src/lib/calc_percentage.ts
+++ src/lib/calc_percentage.ts
@@ -1,18 +1,22 @@
 import type { languageUsage } from "../types/language";
 
-export default function calcPercentage(totals: Map<string, number>): languageUsage[] {
-    const totalBytes = Array.from(totals.values()).reduce((acc, value) => acc + value, 0);
+export default function calcPercentage(
+	totals: Map<string, number>,
+): languageUsage[] {
+	const totalBytes = Array.from(totals.values()).reduce(
+		(acc, value) => acc + value,
+		0,
+	);
 
-    const sorted = Array.from(totals.entries())
-        .map(([language, bytes]) => ({
-            language,
-            bytes,
-            percentage: totalBytes === 0 ? 0 : (bytes / totalBytes) * 100
-        }))
-        .sort((a, b) => b.percentage - a.percentage);
+	const sorted = Array.from(totals.entries())
+		.map(([language, bytes]) => ({
+			language,
+			bytes,
+			percentage: totalBytes === 0 ? 0 : (bytes / totalBytes) * 100,
+		}))
+		.sort((a, b) => b.percentage - a.percentage);
 
+	// 厳密に100%にするための調整は今のところしない
 
-    // 厳密に100%にするための調整は今のところしない
-        
-    return sorted;
+	return sorted;
 }
--- src/types/githubRepo.ts
+++ src/types/githubRepo.ts
@@ -1,11 +1,11 @@
 import type { languageUsage } from "./language";
 
 export type GitHubRepo = {
-    fork: boolean;
-    archived: boolean;
-    languages_url: string;
-}
+	fork: boolean;
+	archived: boolean;
+	languages_url: string;
+};
 
 export type FetchResult =
-    | { ok: true; data: languageUsage[]; fetchedAt: number }
-    | { ok: false; errorMessage: string; statusCode?: number; };
+	| { ok: true; data: languageUsage[]; fetchedAt: number }
+	| { ok: false; errorMessage: string; statusCode?: number };
