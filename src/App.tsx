7346e0c5636cd0ac391f359ed832d46417983d10
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-09-22 14:40:40 +0900

    ci: fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { css } from "../styled-system/css";
 import Access from "./pages/Access";
+
 function App() {
 	return (
 		<div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
--- src/utils/Header.tsx
+++ src/utils/Header.tsx
@@ -1,53 +1,57 @@
 import type { ReactNode } from "react";
 import { css } from "../../styled-system/css";
 
-type IconProps = 
-    | { type: "react-icon"; icon: ReactNode }
-    | { type: "image"; icon: string; alt?: string };
+type IconProps =
+	| { type: "react-icon"; icon: ReactNode }
+	| { type: "image"; icon: string; alt?: string };
 
-export function Header({title, icon}: {title: string; icon: IconProps}) {
-    return (
-        <div
-            className={css({
-                display: "flex",
-                p: 4,
-                alignItems: "center",
-                _hover: {
-                    color: "gray.500"
-                }
-            })}
-        >
-            <span className={css({
-                fontSize: "36px",
-            })}>
-                {title}
-            </span>
-            {icon.type === "react-icon" ? (
-                <span
-                    className={css({
-                        fontSize: "36px",
-                        ml: 8,
-                    })}>
-                    {icon.icon}
-                </span>
-            ) : (
-                <span
-                    className={css({
-                        display: "inline-block",
-                        width: "24px",
-                        height: "24px",
-                    })}>
-                    <img 
-                        src={icon.icon} 
-                        alt={icon.alt ?? title} 
-                        className={css({
-                            width: "100%",
-                            height: "100%",
-                            objectFit: "contain",
-                        })}
-                    />
-                </span>
-            )}
-        </div>
-    )
+export function Header({ title, icon }: { title: string; icon: IconProps }) {
+	return (
+		<div
+			className={css({
+				display: "flex",
+				p: 4,
+				alignItems: "center",
+				_hover: {
+					color: "gray.500",
+				},
+			})}
+		>
+			<span
+				className={css({
+					fontSize: "36px",
+				})}
+			>
+				{title}
+			</span>
+			{icon.type === "react-icon" ? (
+				<span
+					className={css({
+						fontSize: "36px",
+						ml: 8,
+					})}
+				>
+					{icon.icon}
+				</span>
+			) : (
+				<span
+					className={css({
+						display: "inline-block",
+						width: "24px",
+						height: "24px",
+					})}
+				>
+					<img
+						src={icon.icon}
+						alt={icon.alt ?? title}
+						className={css({
+							width: "100%",
+							height: "100%",
+							objectFit: "contain",
+						})}
+					/>
+				</span>
+			)}
+		</div>
+	);
 }
